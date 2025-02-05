9b3d709dc252ba8a0ad7475c6655ea4366f7dcf0
Warlyn Piña <88066951+warlynjohan026@users.noreply.github.com>
2025-02-05 13:04:04 -0400

    Final work

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Let's check this")
+const Truthytrues = {
+    names: ["Calors", "Juan", "Pedro", "Samuel", "Karla", "Samantha"],
+    true: ["Cree esto"],
+    verse: ['1 Thessalonians 5:16: "Rejoice always."', '1 Thessalonians 5:18: "Give thanks always."', '1 John 4:8: "God is love."', 'Romans 12:12: "Rejoicing in hope."', '1 Thessalonians 5:17: "Pray without ceasing."']
+
+}
+
+const getRandomItem = array => {
+    return array[Math.floor(Math.random() * array.length)]
+}
+
+
+const generateRandomMessage = data => {
+    const [name, verdad, verso] = Object.values(data).map(getRandomItem) 
+    return `Hey ${name}, ${verdad} en ${verso} Dios te bendiga!`
+}
+
+
+//console.log(generateRandomMessage(Truthytrues))
+console.log(getRandomItem(["Calors", "Juan", "Pedro", "Samuel", "Karla", "Samantha"]))
